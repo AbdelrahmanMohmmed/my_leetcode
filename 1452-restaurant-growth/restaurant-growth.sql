23b96228d2192ac5ace2683f1ef0c9f5e7306195
AbdelrahmanMohmmed <121166488+AbdelrahmanMohmmed@users.noreply.github.com>
2026-05-16 18:21:14 +0300

    Time: 354 ms (62.70%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,30 @@
+WITH daily AS
+(
+    SELECT 
+        visited_on,
+        SUM(amount) AS amount
+    FROM Customer
+    GROUP BY visited_on
+),
+
+cte AS
+(
+    SELECT
+        visited_on,
+        SUM(amount) OVER (
+            ORDER BY visited_on
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        ) AS amount,
+        
+        ROW_NUMBER() OVER (
+            ORDER BY visited_on
+        ) AS rn
+    FROM daily
+)
+
+SELECT
+    visited_on,
+    amount,
+    ROUND(amount / 7, 2) AS average_amount
+FROM cte
+WHERE rn >= 7;
